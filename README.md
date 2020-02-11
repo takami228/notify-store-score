d28a46168cc6c649851eb9d919dc96c22dfdfea9
takami228 <m.takami228@gmail.com>
2020-02-11 16:56:25 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# notify-store-score
+# notify store score
+
+This application post your mobile application's scores distributed in Goole Play and AppStore using npm modules([google-play-scraper](https://www.npmjs.com/package/google-play-scraper), [app-store-scraper](https://www.npmjs.com/package/app-store-scraper)).
+
+# Requirements
+
+- Node.js
+- Slack or Mattermost webhook URL
+  - [Slack](https://my.slack.com/services/new/incoming-webhook)
+  - [Mattermost](https://docs.mattermost.com/developer/webhooks-incoming.html)
+
+# Settings
+
+Set envirment variables in `.env` file.
+
+|name|value|Example|
+|---|---|---|
+|ANDROID_APP_ID| Your Android App Id | com.example.yourapp |
+|IOS_APP_ID| Your iOS App Id | 123456789 (without prefix "id")|
+|DIALECT| Notification Chat Application | `slack` or `mattermost`|
+|WEBHOOK_URL| slack or mattermost webhook url | https://hooks.slack.com/services/xxxx/yyyy <br> https://mattermost.hostname/hooks/xxxxxx |
+|UERNAME| Notification usename in your Chat Application | StoreScoreNotifier |
+|CHANNEL| Notification Channel in your Chat Application | bot|
+
+```
+# .env file
+ANDROID_APP_ID=com.example.yourapp
+IOS_APP_ID=123456789
+DIALECT=slack
+WEBHOOK_URL=https://hooks.slack.com/services/xxxx/yyyy
+USERNAME=StoreScoreNotifier
+CHANNEL=bot
+```
+
+# How to run
+
+Install packages and run apps.
+
+```
+# install packages
+$ package install
+
+# run
+$ node app.js
+```
+
+# Daily CI settings
+
+You can set this application in CI Runner and set daily job.
+
+## GitLab CI Sample
+
+```yml
+dailyScoreNotifier:
+  stage: build
+  tags:
+    - ci-runner-name
+  script:
+    - npm install
+    - node app.js
+  only:
+    - schedules
+```
+
+# ToDo
+
+- Dockerize application 
---
+++ app.js
@@ -0,0 +1,96 @@
+const googlePlay = require('google-play-scraper');
+const appStore = require('app-store-scraper');
+const request = require('request');
+const dateFormat = require('dateformat');
+require('dotenv').config();
+
+function postMessage(dialect, webhookUrl, username, channel, text){
+    if(dialect == "slack"){
+        postMessageToSlack(webhookUrl, username, channel, text);
+    } else if(dialect == "mattermost"){
+        postMessageToMattermost(webhookUrl, username, channel, text);
+    } else {
+        console.log("dialet should be 'slack' or 'mattemost'.");
+    }
+}
+
+function postMessageToSlack(webhookUrl, username, channel, text){
+    const options = {
+        url: webhookUrl,
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        json: true,
+        body: {
+            "username": username,
+            "channel": channel,
+            "attachments": [
+                {"text": text}
+            ]
+        }
+    }
+    request(options, function(error, response, body){
+        console.log(error);
+    });
+}
+
+function postMessageToMattermost(webhookUrl, username, channel, text){
+    const options = {
+        url: webhookUrl,
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        json: true,
+        body: {
+            "username": username,
+            "channel": channel,
+            "text": text
+        }
+    }
+    request(options, function(error, response, body){
+        console.log(error);
+    });
+}
+
+function constructPostText(dialect, appScores){
+    const now = new Date();
+    const dateString = dateFormat(now, "yyyy年mm月dd日");
+    var text;
+    if(dialect == "slack"){
+        text = "*" + dateString + "のストア評価*\n"
+        + "Android : " + appScores["android"]["score"] + " (" + appScores["android"]["reviews"] + ")\n"
+        + "iOS : " + appScores["ios"]["score"] + " (" + appScores["ios"]["reviews"] + ")"; 
+    } else if(dialect == "mattermost") {
+        text = "#### " + dateString + "のストア評価\n\n" 
+        + "|OS|Score|Reviews|\n|---|---|---|\n" 
+        + "|Android|" + appScores["android"]["score"] + "|" + appScores["android"]["reviews"] + "|\n"
+        + "|iOS|" + appScores["ios"]["score"] + "|" + appScores["ios"]["reviews"] + "|\n"; 
+    } else {
+        text = "Error...";
+        console.log("dialet should be 'slack' or 'mattemost'.");
+    }
+    return text;   
+}
+
+const androidAppId = process.env.ANDROID_APP_ID;
+const iosAppId = process.env.IOS_APP_ID;
+const dialect = process.env.DIALECT;
+const webhookUrl = process.env.WEBHOOK_URL;
+const username = process.env.USERNAME;
+const channel = process.env.CHANNEL;
+
+const androidReview = googlePlay.app({appId: androidAppId, country: 'jp'})
+const iosReview = appStore.app({id: iosAppId, country: 'jp'});
+
+Promise.all([androidReview, iosReview]).then(function(values){
+    const appScores = {
+        'android' :  {
+            'score' : values[0]['score'],
+            'reviews' : values[0]['reviews']
+        },
+        'ios' : {
+            'score' : values[1]['score'],
+            'reviews' : values[1]['reviews']
+        }
+    };
+    const text = constructPostText(dialect, appScores);
+    postMessage(dialect, webhookUrl, username, channel, text);
+});
